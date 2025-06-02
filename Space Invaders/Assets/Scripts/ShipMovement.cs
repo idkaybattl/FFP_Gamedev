a8362e65501e091501a33c04079b4b8e13ad42e9
mo <moritz.fechte@gmail.com>
2025-06-02 23:22:50 +0200

    rotation with a and d, movement with w and s

--- Space Invaders/Assets/Scripts/ShipMovement.cs
+++ Space Invaders/Assets/Scripts/ShipMovement.cs
@@ -6,37 +6,54 @@ using UnityEngine.Rendering;
 
 public class ShipMovement : MonoBehaviour
 {
-    public Vector2 velocity = new();
+    public Vector3 velocity = new();
     public float acceleration;
     public float velocityCap;
     public float friction;
-    public Vector3 direction;
-    Camera cam;
+    public float rotationVelocity;
+    public float rotationAcceleration;
+    public float rotationVelocityCap;
+    public float rotationFriction;
     Vector3 mouseWorldSpace;
     InputAction moveAction;
     Vector2 inputMoveVector;
+    Vector3 moveVector;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
-        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        inputMoveVector = moveAction.ReadValue<Vector2>();
+
         // rotate ship
+        if (inputMoveVector.x != 0 || rotationVelocity > 0.05 || rotationVelocity < -0.05)
+        {
+            Debug.Log("yay");
+            rotationVelocity += inputMoveVector.x * rotationAcceleration * Time.deltaTime;
+            rotationVelocity = Mathf.MoveTowards(rotationVelocity, 0f, rotationFriction * Time.deltaTime);
+
+            rotationVelocity = Mathf.Clamp(rotationVelocity, -rotationVelocityCap, rotationVelocityCap);
+
+            transform.Rotate(0, 0, -rotationVelocity * Time.deltaTime);
+        }
+        else
+        {
+            rotationVelocity = 0;
+        }
 
 
         // move ship
-        inputMoveVector = transform.TransformDirection(moveAction.ReadValue<Vector2>());
+        moveVector = transform.up * inputMoveVector.y;
 
-        if (inputMoveVector != new Vector2() || velocity.magnitude > 0.05)
+        if (inputMoveVector.y != 0 || velocity.magnitude > 0.05)
         {
-
-            velocity += inputMoveVector * (acceleration + friction) * Time.deltaTime;
+            velocity += moveVector * acceleration * Time.deltaTime;
             velocity += velocity.normalized * -friction * Time.deltaTime;
 
             velocity = Vector2.ClampMagnitude(velocity, velocityCap);
