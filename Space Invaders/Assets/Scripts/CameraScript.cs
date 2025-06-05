478817377070b2d0f948e3dc7e7f68f9c532c4cf
mo <moritz.fechte@gmail.com>
2025-06-05 21:50:30 +0200

    basically a racing game
    yup

---
+++ Space Invaders/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject ship;
+    public float camSpeedCap;
+    float velocity;
+    Vector3 shipVector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
