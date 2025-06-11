fbec90cc915cc78948f612fe723e194e79dadb75
mo <moritz.fechte@gmail.com>
2025-06-12 00:34:22 +0200

    more ideas no time

--- Space Invaders/Assets/Scripts/CameraScript.cs
+++ Space Invaders/Assets/Scripts/CameraScript.cs
@@ -4,17 +4,21 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject ship;
     ShipMovement shipMovement;
+    public float zoomSpeed;
+    public float zoomRecovery;
     public float maxZoomOut;
+    float originalZoom;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         shipMovement = ship.GetComponent<ShipMovement>();
+        originalZoom = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float smth = shipMovement.velocity.magnitude * maxZoomOut / shipMovement.velocityCap;
+        Camera.main.orthographicSize = originalZoom + shipMovement.velocity.magnitude * maxZoomOut / shipMovement.velocityCap;
     }
 }
