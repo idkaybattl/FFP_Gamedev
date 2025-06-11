213103cdf11840f6e880f5c13d05720ee53c31fb
mo <moritz.fechte@gmail.com>
2025-06-12 00:18:58 +0200

    setup for camera zoom on speed change

--- Space Invaders/Assets/Scripts/CameraScript.cs
+++ Space Invaders/Assets/Scripts/CameraScript.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject ship;
-    public float camSpeedCap;
-    float velocity;
-    Vector3 shipVector;
+    ShipMovement shipMovement;
+    public float maxZoomOut;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        shipMovement = ship.GetComponent<ShipMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float smth = shipMovement.velocity.magnitude * maxZoomOut / shipMovement.velocityCap;
     }
 }
