f0c7c9401490721489851db0c8d2ae2d56a49492
mo <moritz.fechte@gmail.com>
2025-06-11 19:40:40 +0200

    new movement
    rotation with a and d and movement with w and s

--- Space Invaders/Assets/Scripts/ShipMovement.cs
+++ Space Invaders/Assets/Scripts/ShipMovement.cs
@@ -7,30 +7,54 @@ public class ShipMovement : MonoBehaviour
     public float acceleration;
     public float velocityCap;
     public float friction;
-    InputAction inputAction;
-    Vector2 input;
+
+    public float rotationVelocity = 0;
+    public float rotationAcceleration;
+    public float rotationVelocityCap;
+    public float rotationFriction;
+
+    InputAction moveAction;
+    InputAction rotateAction;
+    float moveInput;
+    float rotateInput;
     Vector3 moveVector;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        inputAction = InputSystem.actions.FindAction("Move");
+        moveAction = InputSystem.actions.FindAction("Move");
+        rotateAction = InputSystem.actions.FindAction("Rotate");
     }
 
     // Update is called once per frame
     void Update()
     {
-        input = inputAction.ReadValue<Vector2>();
+        moveInput = moveAction.ReadValue<float>();
+        rotateInput = rotateAction.ReadValue<float>();
 
-        
+        // rotate ship
+        if (rotateInput != 0 || Mathf.Abs(rotationVelocity) > 0.05)
+        {
+            rotationVelocity += rotateInput * rotationAcceleration * Time.deltaTime;
+            rotationVelocity = Mathf.MoveTowards(rotationVelocity, 0f, rotationFriction * Time.deltaTime);
+
+            rotationVelocity = Mathf.Clamp(rotationVelocity, -rotationVelocityCap, rotationVelocityCap);
+
+            transform.Rotate(new Vector3(0, 0, -rotationVelocity * Time.deltaTime));
+        }
+        else
+        {
+            rotationVelocity = 0;
+        }
+
         // move ship
-        moveVector = transform.TransformDirection(input);
+        moveVector = transform.up * moveInput;
 
-        if (input.magnitude != 0 || velocity.magnitude > 0.05)
+        if (moveInput != 0 || velocity.magnitude > 0.05)
         {
             velocity += moveVector * acceleration * Time.deltaTime;
-            velocity += velocity.normalized * -friction * Time.deltaTime;
+            velocity -= velocity.normalized * friction * Time.deltaTime;
 
             velocity = Vector2.ClampMagnitude(velocity, velocityCap);
 
--- Space Invaders/Assets/Scripts/Shooting.cs
+++ Space Invaders/Assets/Scripts/Shooting.cs
@@ -3,80 +3,70 @@ using UnityEngine.InputSystem;
 
 public class Shooting : MonoBehaviour
 {
-    // public float shootRate = 0.3f;
-    // public float shootForce;
-    //
-    // float timerRight = 0;
-    // float timerLeft = 0;
-    // public GameObject laser;
-    // public float laserCannonSpacing;
-    // InputAction shootRight;
-    // InputAction shootLeft;
-    // bool right;
-    // bool left;
-    //
-    // float rotationVelocity;
-    // public float rotationDrag;
-    //
-    // ShipMovement movement;
-    // public float knockback;
-    //
-    // // Start is called once before the first execution of Update after the MonoBehaviour is created
-    // void Start()
-    // {
-    //     movement = GetComponent<ShipMovement>();
-    //     shootRight = InputSystem.actions.FindAction("ShootRight");
-    //     shootLeft = InputSystem.actions.FindAction("ShootLeft");
-    // }
-    //
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     right = shootRight.ReadValue<float>() > 0f;
-    //     left = shootLeft.ReadValue<float>() > 0f;
-    //
-    //     if (right && left)
-    //     {
-    //         timerRight = Mathf.Min(timerRight, timerLeft);
-    //         timerLeft = Mathf.Min(timerRight, timerLeft);
-    //     }
-    //
-    //     if (right && timerRight >= shootRate)
-    //     {
-    //         Shoot(1);
-    //         timerRight = 0;
-    //     }
-    //     else
-    //     {
-    //         timerRight += Time.deltaTime;
-    //     }
-    //     if (left && timerLeft >= shootRate)
-    //     {
-    //         Shoot(-1);
-    //         timerLeft = 0;
-    //     }
-    //     else
-    //     {
-    //         timerLeft += Time.deltaTime;
-    //     }
-    //
-    //     if (Mathf.Abs(rotationVelocity) > 0.05)
-    //     {
-    //         rotationVelocity = Mathf.MoveTowards(rotationVelocity, 0f, rotationDrag * Time.deltaTime);
-    //     }
-    //     else
-    //     {
-    //         rotationVelocity = 0;
-    //     }
-    //
-    //     transform.Rotate(new Vector3(0, 0, rotationVelocity * Time.deltaTime));
-    // }
-    //
-    // void Shoot(float sideFactor)
-    // {
-    //     Instantiate(laser, transform.TransformPoint(new Vector3(sideFactor * laserCannonSpacing, 0, 0)), transform.rotation, transform.parent);
-    //
-    //     rotationVelocity += sideFactor * shootForce;
-    //     movement.velocity -= transform.up * knockback;
-    // }
+    public float shootRate = 0.3f;
+    public float shootForce;
+
+    float timerRight = 0;
+    float timerLeft = 0;
+    public GameObject laser;
+    public float laserCannonSpacing;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+//         movement = GetComponent<ShipMovement>();
+//         shootRight = InputSystem.actions.FindAction("ShootRight");
+//         shootLeft = InputSystem.actions.FindAction("ShootLeft");
+    }
+//
+    // Update is called once per frame
+    void Update()
+    {
+//         right = shootRight.ReadValue<float>() > 0f;
+//         left = shootLeft.ReadValue<float>() > 0f;
+//
+//         if (right && left)
+//         {
+//             timerRight = Mathf.Min(timerRight, timerLeft);
+//             timerLeft = Mathf.Min(timerRight, timerLeft);
+//         }
+//
+//         if (right && timerRight >= shootRate)
+//         {
+//             Shoot(1);
+//             timerRight = 0;
+//         }
+//         else
+//         {
+//             timerRight += Time.deltaTime;
+//         }
+//         if (left && timerLeft >= shootRate)
+//         {
+//             Shoot(-1);
+//             timerLeft = 0;
+//         }
+//         else
+//         {
+//             timerLeft += Time.deltaTime;
+//         }
+//
+//         if (Mathf.Abs(rotationVelocity) > 0.05)
+//         {
+//             rotationVelocity = Mathf.MoveTowards(rotationVelocity, 0f, rotationDrag * Time.deltaTime);
+//         }
+//         else
+//         {
+//             rotationVelocity = 0;
+//         }
+//
+//         transform.Rotate(new Vector3(0, 0, rotationVelocity * Time.deltaTime));
+    }
+
+    void Shoot(float sideFactor)
+    {
+//         Instantiate(laser, transform.TransformPoint(new Vector3(sideFactor * laserCannonSpacing, 0, 0)), transform.rotation, transform.parent);
+//
+//         rotationVelocity += sideFactor * shootForce;
+//         movement.velocity -= transform.up * knockback;
+    }
 }
