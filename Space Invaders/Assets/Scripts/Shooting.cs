65289e7800ad3c780bbb590b493be021fd20596c
mo <moritz.fechte@gmail.com>
2025-06-21 18:36:17 +0200

    health icons

--- Space Invaders/Assets/Scripts/Shooting.cs
+++ Space Invaders/Assets/Scripts/Shooting.cs
@@ -16,7 +16,7 @@ public class Shooting : MonoBehaviour
 
     GameObject currentLaser;
     LaserScript currentLaserScript;
-
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
